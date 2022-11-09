c39d7a6a4cc51c9cfa9babdf69c7663c7a1355fb
marcov55 <marcovascogit@gmail.com>
2022-11-09 17:54:53 +0000

    Jogos models

---
+++ app/src/main/java/marcoantonio/controllers/JogosController.java
@@ -0,0 +1,10 @@
+package marcoantonio.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import marcoantonio.repositories.JogoRepository;
+
+@Controller
+public class JogosController {
+    private JogoRepository jogosRepo;
+}
---
+++ app/src/main/java/marcoantonio/repositories/JogoRepository.java
@@ -0,0 +1,9 @@
+package marcoantonio.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import marcoantonio.models.Jogo;
+
+public interface JogoRepository extends CrudRepository<Jogo, Integer> {
+
+}
